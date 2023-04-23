327bc498db990274feb523213584f8f55ab02ca8
Hitansh Thakur <thakurhitansh4325@gmail.com>
2023-04-23 12:47:08 +0530

    removed search box

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,10 +20,12 @@ export const Navbar = () => {
         
         
       </ul>
-      <form className="d-flex" role="search">
+      
+      {/* Search button */}
+      {/* <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      </form> */}
     </div>
   </div>
 </nav>
