e53041df5372ed09d37c6d55c2ccda57802df5b0
Hitansh Thakur <thakurhitansh4325@gmail.com>
2023-07-22 19:49:08 +0530

    Played with the timeout Fuction
    it is still not working

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,15 +3,52 @@ import { useState } from 'react'
 
 const Alert = (props) => {
     const [dismiss, setdismiss] = useState(true)
+    const [progressWidth, setprogressWidth] = useState(0)
+
+
+
+        let MyTimeOut = setTimeout(() => {
+            setprogressWidth(100)
+        }, 3000);
+        
+
+    
 
 
     function handleClick() {
         setdismiss(false)
         props.alert.close()
+        clearTimeout(MyTimeOut)
+
+        MyTimeOut = setTimeout(() => {
+            setprogressWidth(100)
+        }, 3000);
+        
     }
 
         return <>
-            {props && dismiss && <div className={`d-flex alert alert-${props.alert.Type} alert-dismissible show fade`} role="alert"> 
+        <style>
+        {
+          `
+          @keyframes progressAnimation {
+            0%{
+                vi: 0%;
+        
+            }
+            100%{
+                width:100%;
+            }
+        }
+
+        
+        .animate{
+            animation: progressAnimation 2000ms ease;
+        }
+            
+            `
+        }
+        </style>
+            {props && dismiss && <div className={`fixed d-flex alert my-0 alert-${props.alert.Type} alert-dismissible show fade`} role="alert"> 
                 <button type="button" onClick={handleClick} className="btn-close" aria-label="Close"></button>
 
                 {/* material icon for tick success */}
@@ -22,6 +59,10 @@ const Alert = (props) => {
             
         
             </div>}
+            <div className="progress my-0 py-0" role="progressbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{height: 2 + "px"}}>
+                <div className="progress-bar bg-success animate" style={{width:`${progressWidth}%`}}></div>
+            </div>
+
         </>
    
 }
